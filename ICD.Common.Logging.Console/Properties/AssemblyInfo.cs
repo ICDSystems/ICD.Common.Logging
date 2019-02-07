90b152b85d4317198a372d80fca15e665949c4f3
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:32:49 -0500

    chore: Updating AssemblyInfo

--- ICD.Common.Logging.Console/Properties/AssemblyInfo.cs
+++ ICD.Common.Logging.Console/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Logging.Console")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Logging.Console")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("5.0.1.0")]
