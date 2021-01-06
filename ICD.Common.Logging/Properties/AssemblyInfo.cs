8e3169f0f29cf1ff48e4f7990100056595208cff
Chris Cameron <chris.cameron@wedoresi.com>
2021-01-06 16:45:03 -0500

    chore: Updating copyright year

--- ICD.Common.Logging/Properties/AssemblyInfo.cs
+++ ICD.Common.Logging/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Logging.Console")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Logging.Console")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("7.0.0.0")]
